2ee63b3a49ace77e50a7c23b62fc881c7efe7ca3
bakerbd <bakerbd@users.noreply.github.com>
2016-06-14 13:54:55 -0400

    Add files via upload

--- airnow.py
+++ airnow.py
@@ -1,7 +1,7 @@
 # this is written to retrive airnow data concatenate and add to pandas array for usage
 import pandas as pd
 from tools import search_listinlist
-from datetime import datetime
+from datetime import datetime,timedelta
 from numpy import array
 
 
@@ -39,46 +39,62 @@ class airnow:
 
     def download_rawfiles(self, flist):
         if flist.shape[0] < 2:
+            print 'Downloading: ' + flist[0]
             self.download_single_rawfile(flist[0])
         else:
             for i in flist:
+                print 'Downloading: ' + i
                 self.download_single_rawfile(i)
 
-    def retrive_hourly_files(self):
+    def download_hourly_files(self):
+        print 'Connecting to FTP: ' + self.url
         self.openftp()
+        print 'Retrieving Hourly file list'
         nlst = self.retrieve_hourly_filelist()
+        self.convert_dates_tofnames()
         index1, index2 = search_listinlist(array(nlst), array(self.datestr))
-        if index1.shape < 1:
+        if index1.shape[0] < 1:
             self.ftp.cwd('Archive')
             year = self.dates[0].strftime('%Y')
             self.ftp.cwd(year)
             nlst = self.ftp.nlst('*')
             index1, index2 = search_listinlist(array(nlst), array(self.datestr))
             if index1.shape[0] < 1:
-                print 'AirNow does not have hourly data at this time.  Please try again'
+                print 'AirNow does not have hourly data at this time. Please try again'
             else:
-                first = True
-                for i in nlst[index1]:
-                    dft = pd.DataFrame(i, delimiter='|', header=None, infer_datetime_format=True)
-                    cols = ['date', 'time', 'SCS', 'Site', 'utcoffset', 'Species', 'Units', 'Obs', 'Source']
-                    dft.columns = cols
-                    dates = self.calc_datetime(dft.date.values,dft.date.values)
-                    dft['datetime_local'] = dates
-                    if first:
-                        self.output = dft.copy()
-                    else:
-                        self.output = self.output.append(dft)
+                self.download_rawfiles(array(nlst)[array(index1)])
 
-    def load_airnow_file(self, filename='intputfile'):
-        dft = pd.DataFrame(filename, delimiter='|', header=None, infer_datetime_format=True)
-        cols = ['date', 'time', 'SCS', 'Site', 'utcoffset', 'Species', 'Units', 'Obs', 'Source']
-        dft.columns = cols
+        else:
+            self.download_rawfiles(array(nlst)[array(index1)])
 
-    def calc_datetime(self,dates,times):
+    def load_airnow_files(self, fname=''):
+        from glob import glob
+        fnames = array(glob(fname))
+        if fnames.shape[0] < 2:
+            dft = pd.read_csv(fnames[0],delimiter='|', header=None, parse_dates=[[0,1]],infer_datetime_format=True)
+            cols = ['datetime', 'SCS', 'Site', 'utcoffset', 'Species', 'Units', 'Obs', 'Source']
+            dft.columns = cols
+        else:
+            first = True
+            for i in fnames:
+                dft = pd.read_csv(i,delimiter='|', header=None, parse_dates=[[0,1]],infer_datetime_format=True)
+                cols = ['datetime', 'SCS', 'Site', 'utcoffset', 'Species', 'Units', 'Obs', 'Source']
+                dft.columns = cols
+                #dft['datetime_local'] =
+                if first:
+                    self.output = dft.copy()
+                    first=False
+                else:
+                    self.output = self.output.append(dft)
+        self.calc_datetime()
+
+    def calc_datetime(self):
         #takes in an array of string dates and converts to numpy array of datetime objects
         dt = []
-        for i,j in zip(dates,times):
-            string = i + ' ' + j
-            dt.append(datetime.strptime(string,'%y/%d/%m %H:%M'))
-        return array(dt)
-
+        for i in self.output.utcoffset.values:
+            dt.append(timedelta(hours=i))
+        self.output['datetime_local'] = self.output.datetime + dt
+        
+    def set_daterange(self,begin='',end=''):
+        dates = pd.date_range(start=begin,end=end,freq='H').values.astype('M8[s]').astype('O')
+        self.dates = dates
