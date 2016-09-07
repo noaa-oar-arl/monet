b9ff2f0ae3cf16b489d5bfc61f117145c50d913f
bakerbd <barrybaker@icloud.com>
2016-09-07 11:45:40 -0400

    adding OC for pm species

--- cmaq.py
+++ cmaq.py
@@ -309,8 +309,8 @@ class cmaq:
                 print '    Variable ' + i + ' not found'
                 var = zeros((self.indexdates.shape[0],self.latitude.shape[0],self.longitude.shape[0]))
             else:
-                print '   Getting CMAQ Variable: ' + keys[i]
-                var = self.concobj.variables[keys[i]][self.indexdates, 0, :, :].squeeze()
+                print '   Getting CMAQ Variable: ' + i
+                var = self.concobj.variables[i][self.indexdates, 0, :, :].squeeze()
             vars.append(var)
         OC = (vars[0] + vars[1] + vars[2]) / 2.0 + (vars[3] + vars[4] + vars[5]) / 2.0 + (vars[6] + vars[7] + vars[
             8]) / 2.0 + (vars[9] + vars[10]) / 1.6 + vars[11] / 2.7 + (vars[12] + vars[13]) / 1.4 + vars[
