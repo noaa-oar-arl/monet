88b67235c222013d88f547ded71d710e884022a7
Barry Baker <bbakernoaa@users.noreply.github.com>
2016-10-12 14:43:37 -0400

    Update region_compare.py

--- scripts/region_compare.py
+++ scripts/region_compare.py
@@ -1,7 +1,8 @@
 #! /naqfc/noscrub/Barry.Baker/anaconda2/bin/python
-
-
 ###for aitken replace the first line with /data/aqf/barryb/anaconda2/bin/python for the line above
+#   To use this function you exectute as follows
+#
+# ./region_compare.py PATH/TO/ACONC PATH/TO/GRIDCRO LABEL USERNAME PASSWORD STATE
 
 import verify
 from glob import glob
