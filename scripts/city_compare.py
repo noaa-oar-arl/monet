4467633add4594683581ade16e47e6ffdfccc145
Barry Baker <bbakernoaa@users.noreply.github.com>
2016-10-12 14:10:56 -0400

    Update city_compare.py

--- scripts/city_compare.py
+++ scripts/city_compare.py
@@ -21,8 +21,15 @@ files = glob(sys.argv[1])
 grid = sys.argv[2]
 
 va = verify.verify_airnow(concpath=files,gridcro=grid,datapath='.',user=sys.argv[4],passw=sys.argv[5],combine=True,neighbors=9)
-params = va.df.groupby('MSA_Name').get_group(sys.argv[7]).Species.unique()
+
+names = df.MSA_Name.dropna().unique()
+city = sys.argv[7]
+name = ''
+for i in names:
+    if city.upper() in i.upper():
+        name = i
+params = va.df.groupby('MSA_Name').get_group(name).Species.unique()
 for j in params:
-    va.compare_param(param=j,timeseries=True,label=sys.argv[3],state=sys.argv[7],footer=False)
+    va.compare_param(param=j,timeseries=True,label=sys.argv[3],city=name,footer=False)
     plt.savefig(j + '_'+sys.argv[7]+ sys.argv[6],dpi=100)
     plt.close()
