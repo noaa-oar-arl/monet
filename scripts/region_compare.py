ad9e065095baf036d1d3e3b4418b4ca5b559eb29
Barry Baker <bbakernoaa@users.noreply.github.com>
2016-10-12 13:50:50 -0400

    Update region_compare.py

--- scripts/region_compare.py
+++ scripts/region_compare.py
@@ -22,12 +22,10 @@ grid = sys.argv[2]
 va = verify.verify_airnow(concpath=files,gridcro=grid,datapath='.',user=sys.argv[4],passw=sys.argv[5],combine=True,neighbors=9)
 
 for i in va.df.Region.dropna().unique():
-  
+  params = va.df.groupby('Region').get_group(i).Species.unique()
+  for j in params:
+    va.compare_param(param=j,timeseries=True,label=sys.argv[3],region=i)
+    plt.savefig(j + '_'+i+ sys.argv[6],dpi=100)
+    plt.close()
 
-va.compare_param(param='PM2.5',timeseries=True,label=sys.argv[3])
-plt.savefig('pm25_' + sys.argv[6],dpi=100)
-plt.close()
 
-va.compare_param(param='OZONE',timeseries=True,label=sys.argv[3])
-plt.savefig('o3_' + sys.argv[6],dpi=100)
-plt.close()
