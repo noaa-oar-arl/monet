85061d6b4912d96bc334f6afb31a55c9911cab34
Barry Baker <bbakernoaa@users.noreply.github.com>
2016-10-14 08:25:34 -0400

    Create statstable.py

---
+++ scripts/statstable.py
@@ -0,0 +1,6 @@
+#! /naqfc/noscrub/Barry.Baker/anaconda2/bin/python
+###for aitken replace the first line with /data/aqf/barryb/anaconda2/bin/python for the line above
+#   To use this function you exectute as follows
+#
+# ./5x_domain_compare.py PATH/TO/ACONC PATH/TO/GRIDCRO LABEL USERNAME PASSWORD
+
