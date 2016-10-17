828ea8380488d97601b9b0bef2268513039945b9
Barry Baker <bbakernoaa@users.noreply.github.com>
2016-10-17 14:41:48 -0400

    Update 5x_domain_compare.py

--- scripts/5x_domain_compare.py
+++ scripts/5x_domain_compare.py
@@ -17,12 +17,19 @@ print "LABEL: ", sys.argv[3]
 print "AIRNOW Username: ", sys.argv[4]
 print "AIRNOW Password: ", sys.argv[5]
 print "OUTPUT FILENAME: ", sys.argv[6]
+print "SECOND ACONC FILE: ", sys.argv[7]
+print "SECOND ACONC LABEL: ", sys.argv[8]
 
 files = glob(sys.argv[1])
 grid = sys.argv[2]
-va = verify.verify_airnow(concpath=files,gridcro=grid,datapath='.',user=sys.argv[4],passw=sys.argv[5],combine=True,neighbors=9)
+va = verify.verify_airnow(concpath=files,gridcro=grid,datapath='.',user=sys.argv[4],passw=sys.argv[5],combine=True,neighbors=10)
+if len(sys.argv[7]) > 4:
+  files = files = glob(sys.argv[7])
+  va2 = verify.verify_airnow(concpath=files,gridcro=grid,datapath='.',user=sys.argv[4],passw=sys.argv[5],combine=True,neighbors=10)
 params = va.df.Species.unique()
 for i in params:
-  va.compare_param(param=i,timeseries=True,label=sys.argv[3])
+  va.compare_param(param=i,timeseries=True,label=sys.argv[3],footer=False)
+  if len(sys.argv[7]) > 4:
+    va2.compare_param(param=i,timeseries=True,label=sys.argv[8],footer=False,fig=plt.figure(1))
   plt.savefig(i.replace('.','')+ '_'+sys.argv[6],dpi=100)
   plt.close()
