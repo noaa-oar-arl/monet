5d7ba7d1fcd38fc391ad65c05e8a762d1f06261d
Barry Baker <bbakernoaa@users.noreply.github.com>
2016-11-16 12:54:37 -0500

    Create gmu_functions.py

---
+++ scripts/gmu_functions.py
@@ -0,0 +1,7 @@
+#!/user/bin/python
+from numpy import *
+import matplotlib.pyplot as plt
+from seaborn import seaborn
+from netCDF4 import MFDataset
+import xarray as xr
+from datetime import datetime
