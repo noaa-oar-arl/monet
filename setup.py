a1bb67affab1fd333c7105c51dcdacb3af801981
bbakernoaa <barry.baker@noaa.gov>
2018-02-02 10:36:12 -0500

    install fixes

--- setup.py
+++ setup.py
@@ -5,24 +5,9 @@ import sys
 from warnings import warn
 
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except:
     from distutils.core import setup
-
-
-def find_packages():
-    import os
-    packages = []
-    walker = os.walk('monet')
-    prefix = os.path.join(os.path.curdir, 'monet')
-    for thisdir, itsdirs, itsfiles in walker:
-        if '__init__.py' in itsfiles:
-            packages.append(thisdir[len(prefix) - 1:])
-
-    return packages
-
-
-packages = find_packages()
 
 setup(
     name='monet',
@@ -33,8 +18,7 @@ setup(
     author_email='barry.baker@noaa.gov',
     maintainer='Barry Baker',
     maintainer_email='barry.baker@noaa.gov',
-    packages=packages,
-    package_dir={'': 'src'},
+    packages=find_packages,
     keywords=['model','verification','hysplit','cmaq','atmosphere','camx','evaluation'],
     description='The Model and Observation Evaluation Toolkit (MONET)',
     install_requires=['numpy', 'pandas', 'wget', 'pyresample', 'netcdf4', 'pynio', 'xarray', 'dask', 'matplotlib', 'seaborn', 'pseudonetcdf'],
