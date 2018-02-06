10f14e43746dcaefeda7d624efac19ac71e2d9f5
bbakernoaa <barry.baker@noaa.gov>
2018-02-06 16:23:00 -0500

    take out hysplit utils

--- monet/models/__init__.py
+++ monet/models/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import, print_function
 
 from . import camx, cmaq, hysplit, hysplit_util
 
-__all__ = ['camx', 'cmaq', 'hysplit', 'hysplit_util']
+__all__ = ['camx', 'cmaq']
 
 __name__ = 'models'
