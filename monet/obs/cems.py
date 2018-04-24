c1028e2a3cac8c5775d942dffa4dd47891357c27
amcz <alicec@umd.edu>
2018-04-24 14:15:46 -0400

    in cems.py: load method can now read a csv file. The csv file dates are not clear (local time but maybe not daylight savings?)
    added capability to rename columns to standard names. Use a hash to keep
    track of how new names correspond to original names.
    
    method create_location_dictionary added.
    
    get_var method can now return pivot table.
    
    filter_by_time method added.

--- monet/obs/cems.py
+++ monet/obs/cems.py
@@ -3,6 +3,16 @@ import os
 import pandas as pd
 import numpy as np
 import datetime
+import pytz
+from timezonefinder import TimezoneFinder
+import sys
+
+
+#def get_utcoffset((lat,lon), date):
+#    timezone = tzwhere.tzwhere()
+#    timezone_str = tzwhere.tzNameAt(lat, lon)
+#    timezone = pytz.timezone(timezone_str)
+#    return timezone.utcoffset(date)
 
 def getdegrees(degrees, minutes, seconds):
         return degrees+ minutes/60.0 + seconds/3600.00
@@ -39,7 +49,8 @@ class CEMSEmissions(object):
       self.lb2kg = 0.453592  #number of kilograms per pound.
       self.info = "Data from continuous emission monitoring systems (CEMS) \n"
       self.info += self.url + '\n'
-      self.df = None
+      self.df = pd.DataFrame()
+      self.namehash = {}  #if columns are renamed keeps track of original names.
       ##Each facility may have more than one unit which is specified by the unit id.
 
 
@@ -85,6 +96,37 @@ class CEMSEmissions(object):
                 url = self.retrieve(rd, st, download=download, verbose=verbose)
                 self.load(url, verbose=verbose)
 
+
+    def filter_by_time(self, df, daterange):
+          """ returns dataframe with values between (and including) [date1, date2]"""
+          if not daterange: return df.copy()
+          temp = df.copy()
+          print('filter by time')
+          print(daterange[0])
+          print(daterange[1])
+          temp = temp[temp['time']>=daterange[0]] 
+          temp = temp[temp['time']<=daterange[1]] 
+          #sys.exit()
+          return temp
+
+
+    def match_column(self, varname):
+        """varname is list of strings.
+           returns column name which contains all the strings.
+        """
+        columns =  list(self.df.columns.values)
+        cmatch = None
+        for ccc in columns:
+            #print('-----'  + ccc + '------')
+            #print( temp[ccc].unique())
+            match=0
+            for vstr in varname:
+                if vstr.lower() in ccc.lower():
+                   match+=1
+            if match == len(varname):
+               cmatch = ccc
+        return cmatch 
+ 
     def get_var(self, varname, loc=None, daterange=None, unitid=-99, verbose=True):
         """returns time series with variable indicated by varname.
            varname may be string or list of strings.
@@ -112,31 +154,40 @@ class CEMSEmissions(object):
         columns =  list(self.df.columns.values)
         #temp = self.df['OP_DATE', 'OP_HOUR', 'OP_TIME']
         #print(temp[0:10])
+       
         if loc:
-            temp = self.df[self.df['FAC_ID'].isin(loc)]
-        if daterange:
-            temp = temp[temp['datetime']>=daterange[0]] 
-            temp = temp[temp['datetime']<=daterange[1]] 
-        for ccc in columns:
-            #print('-----'  + ccc + '------')
-            #print( temp[ccc].unique())
-            match=0
-            for vstr in varname:
-                if vstr.lower() in ccc.lower():
-                   match+=1
-            if match == len(varname):
-               cmatch = ccc
-        ##create pandas frame with index datetime and columns for value for each unit_id
-        pivot = pd.pivot_table(temp, values=cmatch, index=['datetime'], columns=['UNIT_ID'])
-        pivot = pivot.fillna(value=0)
-        #print('------------------pivot')
-        #print(pivot[0:10])
-        if unitid == -99:
-            pivot['all'] = pivot.sum(axis=1)
-            #print(pivot[0:10])
-            return pivot['all']       
+            temp = self.df[self.df['fac_id'].isin(loc)]
         else:
-            return pivot['unitid']  
+            temp = self.df.copy()
+        temp = self.filter_by_time(temp, daterange)
+        cmatch = self.match_column(varname)
+
+        if 'unit_id' in columns:
+            ##create pandas frame with index datetime and columns for value for each unit_id
+            pivot = pd.pivot_table(temp, values=cmatch, index=['time'], columns=['unit_id'])
+            pivot = pivot.fillna(value=0)
+            #print('------------------pivot')
+            #print(pivot[0:10])
+            if unitid == -99:
+                pivot['all'] = pivot.sum(axis=1)
+                #print(pivot[0:10])
+                return pivot['all']       
+            else:
+                return pivot['unitid']  
+        else:
+            #temp.set_index('time', inplace=True)
+            ##returns data frame where rows are date and columns are the values of cmatch for each fac_id.
+            pivot = pd.pivot_table(temp, values=cmatch, index=['time'], columns = ['fac_id'])
+            ldict = self.create_location_dictionary()
+            if ldict:
+                cnew = []
+                columns =  list(pivot.columns.values)
+                for ccc in columns:
+                    cnew.append(ldict[ccc])
+            
+            pivot.columns = cnew
+            return pivot 
+            #return temp[cmatch]
 
     def retrieve(self, rdate, state, download=True, verbose=False):
         """rdate - datetime object. Uses year and month. Day and hour are not used.
@@ -162,6 +213,17 @@ class CEMSEmissions(object):
         self.info += 'File retrieved :' +  efile + '\n'
         return efile
 
+    def create_location_dictionary(self):
+        if 'latitude' in list(self.df.columns.values):
+            dftemp = self.df.copy()
+            pairs = zip(dftemp['fac_id'], zip( dftemp['latitude'], dftemp['longitude']))
+            pairs = list(set(pairs))
+            lhash = dict(pairs)  #key is facility id and value is name.
+            print(lhash)
+            return  lhash
+        else:
+            return false 
+
     def get_location(self, facid):
         """Need to create a comprehensive dictionary for all locations in the US."""
         lhash = {}
@@ -174,20 +236,82 @@ class CEMSEmissions(object):
         lhash[1116]   = (getdegrees(39,35,46), getdegrees(-78,44,46)) #warrier run
         lhash[1229]   = (38.670, -76.865) #brandywine
         lhash[316]   = (39.238,-76.5119) #perryman
+        ##TO DO. This should be moved elsewhere.
+        if 'latitude' in list(self.df.columns.values):
+            dftemp = self.df.copy()
+            pairs = zip(dftemp['fac_id'], zip( dftemp['latitude'], dftemp['longitude']))
+            pairs = list(set(pairs))
+            lhash = dict(pairs)  #key is facility id and value is name.
+            print(lhash)
         self.lhash= lhash
         return lhash[facid]
 
+    def rename(self, ccc, newname, rcolumn, verbose):
+         self.namehash[newname] = ccc  #dictionary with key as the newname and value as the original name
+         rcolumn.append(newname)
+         if verbose: print(ccc + ' to ' + newname)
+         return rcolumn
+
+    def columns_rename(self, columns, verbose=False):
+        rcolumn = []
+        for ccc in columns:
+            if 'facility' in ccc.lower() and 'name' in ccc.lower():
+                rcolumn = self.rename(ccc, 'facility_name', rcolumn, verbose)
+            elif 'facility' in ccc.lower() and 'id' in ccc.lower():
+                rcolumn = self.rename(ccc, 'fac_id', rcolumn, verbose)
+            elif 'so2' in ccc.lower() and ('lbs' in ccc.lower() or 'pounds' in ccc.lower()):
+                rcolumn = self.rename(ccc, 'so2_lbs', rcolumn, verbose)
+            elif 'nox' in ccc.lower() and ('lbs' in ccc.lower() or 'pounds' in ccc.lower()):
+                rcolumn = self.rename(ccc, 'nox_lbs', rcolumn, verbose)
+            elif 'co2' in ccc.lower() and ('short' in ccc.lower() and 'tons' in ccc.lower()):
+                rcolumn = self.rename(ccc, 'co2_short_tons', rcolumn, verbose)
+            elif 'date' in ccc.lower():
+                rcolumn = self.rename(ccc, 'date', rcolumn, verbose)
+            elif 'hour' in ccc.lower():
+                rcolumn = self.rename(ccc, 'hour', rcolumn, verbose)
+            elif 'lat' in ccc.lower():
+                rcolumn = self.rename(ccc, 'latitude', rcolumn, verbose)
+            elif 'lon' in ccc.lower():
+                rcolumn = self.rename(ccc, 'longitude', rcolumn, verbose)
+            elif 'state' in ccc.lower():
+                rcolumn = self.rename(ccc, 'state_name', rcolumn, verbose)
+            else:
+                rcolumn.append(ccc.strip().lower())
+        return rcolumn
+
+    def get_date_fmt(self, date):
+        print(date)
+        temp = date.split('-')
+        if len(temp[0]) == 4:
+           fmt = "%Y-%m-%d %H"
+        else:
+           fmt = "%m-%d-%Y %H"
+        return fmt
+
+    def latlonfilter(self, llcrnr, urcrnr):
+        lat1 = llcrnr[0]
+        lat2 = urcrnr[0]
+        lon1 = llcrnr[1]
+        lon2 = urcrnr[1]
+        self.df =  self.df[self.df['latitude'] < lat2]
+        self.df =  self.df[self.df['latitude'] > lat1]
+        self.df =  self.df[self.df['longitude'] > lon1]
+        self.df =  self.df[self.df['longitude'] < lon2]
 
     def load(self, efile, verbose=True):
         """loads information found in efile into a pandas dataframe.
         """
-        dftemp = pd.read_csv(efile)
+        dftemp = pd.read_csv(efile, sep=',', index_col=False, header=0)
         columns = list(dftemp.columns.values)
+        columns = self.columns_rename(columns, verbose)
+        dftemp.columns = columns
         ckeep=[]
         for ccc in columns:
-            #print('-------------')
-            #print(ccc)
-            #print( dftemp[ccc].unique())
+            if verbose:
+                print('-------------')
+                print(ccc)
+                print(dftemp[ccc][0])
+                #print( dftemp[ccc].unique())
             if 'so2' in ccc.lower():
                 ckeep.append(ccc) 
             elif 'time' in ccc.lower():
@@ -202,28 +326,44 @@ class CEMSEmissions(object):
                 ckeep.append(ccc) 
             elif 'btu' in ccc.lower():
                 ckeep.append(ccc) 
-        dftemp = dftemp[ckeep]
-        cnan = ['SO2_MASS (lbs)']
+        #dftemp = dftemp[ckeep]
+        #cnan = ['SO2_MASS (lbs)']
         ##drop rows with NaN in the cnan column.
-        dftemp.dropna(axis=0,  inplace=True, subset=cnan)
+        #dftemp.dropna(axis=0,  inplace=True, subset=cnan)
         #print(dftemp['FACILITY_NAME'].unique())
         #print(dftemp['FAC_ID'].unique())
-        pairs = zip(dftemp['FAC_ID'], dftemp['FACILITY_NAME'])
-        pairs = list(set(pairs))
+        #pairs = zip(dftemp['fac_id'], dftemp['facility_name'])
+        #pairs = list(set(pairs))
         #print(pairs)
-        self.namehash = dict(pairs)  #key is facility id and value is name.
-
+        #self.namehash = dict(pairs)  #key is facility id and value is name.
+        dfmt = self.get_date_fmt(dftemp['date'][0])
         ##create column with datetime information from column with month-day-year and column with hour.
-        dftime = dftemp.apply(lambda x:pd.datetime.strptime("{0} {1}".format(x['OP_DATE'], x['OP_HOUR'])  , "%m-%d-%Y %H"), axis=1) 
+        dftime = dftemp.apply(lambda x:pd.datetime.strptime("{0} {1}".format(x['date'], x['hour'])  , dfmt), axis=1) 
         dftemp = pd.concat([dftime, dftemp], axis=1) 
-        dftemp.rename(columns={0:'datetime'}, inplace=True)
-        dftemp.drop(['OP_DATE', 'OP_HOUR'], axis=1, inplace=True)
+        dftemp.rename(columns={0:'time local'}, inplace=True)
+        dftemp.drop(['date', 'hour'], axis=1, inplace=True)
 
+        ##TO DO This is a kluge because these dates are when the time change occurs and routine can't handle
+        ## non-existent or ambiguous local times.
+        dftemp= dftemp.ix[dftemp['time local'] != pd.Timestamp(2016,3,13,2)] 
+        dftemp= dftemp.ix[dftemp['time local'] != pd.Timestamp(2016,11,6,1)] 
+
+        tz =  TimezoneFinder()
+        def timezone(x): return tz.timezone_at(lat=x['latitude'], lng=x['longitude'])
+        dftemp['timezone'] = dftemp.apply(timezone, axis=1)
+     
+        ##this creates a column with time stamps which are offset-aware.        
+        def utc(x): return  pd.Timestamp(x['time local'], tz= x['timezone']).tz_convert('UTC')
+        dftemp['time'] = dftemp.apply(utc, axis=1)
+ 
+ 
+        if ['year'] in columns: dftemp.drop(['year'], axis=1, inplace=True)
         if self.df is None:
             self.df = dftemp
             if verbose: print('Initializing pandas dataframe. Loading ' + efile)
         else:
             self.df = self.df.append(dftemp)        
             if verbose: print('Appending to pandas dataframe. Loading ' + efile)
+        if verbose: print(dftemp[0:10])
         return dftemp
         
---
+++ monet/verification/obsdata.py
@@ -0,0 +1,206 @@
+from __future__ import print_function
+import os
+import pandas as pd
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+import sys
+
+def ppb2ugm3(ppb, mw, pressure=None, temperature=None):
+    """ppb is value in volume fraction parts per billion.
+       mw is molecular weight in g/ mol
+       if pressure and temperature are not given then standard pressure and temperature are used"""
+    if not temperature:
+       temperature = 293.0 ##Kelvin.
+    if not pressure:
+        standard_press = 101.325 #(kPa)
+    R = 8.3144 #J/ (mol K)  or (L kPa / (mol K)) 
+    return ppb * mw / R / temperature * pressure
+
+
+def lbs2kg(lbs):
+    kg = 0.453592 * lbs
+    return kg
+
+class StationData(object):
+    """ Class for manipulating data in MONET observation dataframe.
+
+        write_datem:  method for writing text file in datem format to be used with c2datem (HYSPLIT distribution)
+    
+        find_near: finds sites within a certain distance of  an input lat lon location.
+    """
+    def __init__(self, df=None):
+      """input a dataframe"""
+      #self.efile = None   
+      #self.url = None
+      self.lb2kg = 0.453592  #number of kilograms per pound.
+      self.info = None
+      self.df = df
+
+    def find_near(self, latlon, distance=100, sid='site_num', drange=None):
+        """find all values in column sid which are within distance (km) of lat lon point.
+           output dictionary with key as value in column sid and value tuple (latitude, longitude)"""
+        degree2km = 111
+        if drange:
+           df = self.df.copy()
+           self.timefilter(drange)
+        lhash = self.get_lhash(sid)
+        for key in lhash.keys:
+            xd = (lhash[key][1] - latlon[1]) * degree2km *np.cos(latlon[1] * np.pi/ 180.0)
+            yd = (lhash[key][0] - latlon[0]) * degree2km
+            dd = (xd**2 + yd**2) **0.5
+            if dd > distance:
+               lhash.pop(key, None) 
+        return lhash
+
+    def convert_units(self, obscolumn='obs', unit='UG/M3'):
+        ##SO2 see 40 CFR Part 50.5, Appendix A-1 to part 50, appendix A=2 to Part 50.
+        ##to convert from ppb to ug/m3 multiply by 2.6178.
+        factor = 2.6178
+        self.df = self.df[self.df['units']=='ppb']
+        self.df['units'] = 'UG/M3'
+        self.df[obscolumn] = self.df[obscolumn] * 2.6178
+        #self.df[obscolumn] = self.df.apply(lambda x: ppb2ugm3(mw, x[obscolumn]))
+        #self.df['units'] = 'ugm3'
+
+    def write_datem(self, obscolumn='obs', dname = 'datemfile.txt', sitename='1', info=None, drange=None):
+        """returns string in datem format (See NOAA ARL).
+        dname is a string which is the name of the output file.
+        sitename is a string. If it is the name of a column in the dataframe then 
+                  that column will be used to generate the site name column in the datem file.
+                 If is not the name of a column, then the string will be used as the site name.
+        info is a string that will be written to the second line of the header.
+        drange is list of two time stamp objects.
+        Year, Month, Day, Hour, Duration, lat, lon, Concentration (units), site id, height
+        """
+        if drange:
+           df = self.df.copy()
+           self.timefilter(drange)
+          
+        units=self.df['units'].tolist()
+        units = list(set(units))
+        sdate = datetime.datetime(2010,1,1,0)
+        if len(units) > 1: print('WARNING, more than one type of unit ', units)
+        ustr=''
+        for uuu in units:
+            ustr += uuu + ' '
+        runstring =  "Beginning date " + sdate.strftime("%Y %m %d %H:%M") + " UTC ---"
+        runstring += 'Information '  
+        if info:
+           runstring += info + "\n"
+        else:
+           runstring +=  "\n"
+        runstring += "Year, Month, Day, Hour:Minute (UTC), Dur(hhmm) ,  LAT, LON, Concentration (" + ustr +  "), sid, height\n"
+        lat = self.df['latitude']
+        lon = self.df['longitude']
+        cval = self.df[obscolumn]
+        #print t2
+        t1 = self.df['time']
+        duration = ' 0100 '
+        height = '20'
+        if sitename in self.df.columns.values:
+           sval = self.df[sitename]
+        else:
+           sval = [sitename] * len(cval)
+        for val in zip(t1, lat, lon, cval, sval):
+            runstring += val[0].strftime('%Y  %m  %d  %H%M') +  duration
+            print(runstring)
+            try:
+                runstring += str(val[1]) + ' ' + str(val[2]) + ' ' 
+            except:
+                print('WARNING1', val[1])
+                print(val[2])
+                print(type(val[1]))
+                print(type(val[2]))
+                sys.exit()
+            if isinstance(val[4],str):
+                runstring +=  "{:.3f}".format(val[3]) + val[4] + height + "\n"
+            else:
+                runstring +=  "{:.3f}".format(val[3]) + ' ' + "{0:d}".format(val[4]) + ' ' + height + "\n"
+
+        with open(dname, 'w') as fid:
+             fid.write(runstring)
+        if drange: self.df = df  #restore self.df to full dataframe.
+        return runstring
+
+
+    def dropna(self):
+        """remove columns which have all Nans"""
+        self.df = self.df.dropna(axis=1, inplace=True)
+
+
+    def get_lhash(self, idn ):
+        """returns a dictionary with the key as the input column value and the value a tuple of (lat, lon)
+           Useful for getting lat lon locations of different sites in a dataframe.
+        """
+        if 'latitude' in list(self.df.columns.values):
+            dftemp = self.df.copy()
+            pairs = zip(dftemp[idn], zip( dftemp['latitude'], dftemp['longitude']))
+            pairs = list(set(pairs))
+            lhash = dict(pairs)  #key is facility id and value is name.
+            print(lhash)
+        self.lhash= lhash
+        return lhash
+
+
+    def to_hdf(self, hdfname):
+        #not working very well. hdf file too large.
+        store = pd.HDFStore(hdfname)
+        store.put('df', self.df) 
+        #self.df.to_hdf(hdfname, 'key_to_store', format='fixed', mode='w')
+
+    def from_hdf(self, hdfname):
+        store = pd.HDFStore(hdfname)
+        self.df = store['df']
+
+    def __str__(self):
+        return self.info
+
+    def summarize(self, verbose=False):
+        """prints list of columns. if verbose prints list of unique values in each column"""
+        columns = list(self.df.columns.values)
+        if verbose: 
+            for ccc in columns:
+                print(ccc)
+                print(self.df[ccc].unique())
+        print('-------------------------------')
+        for ccc in columns:
+            print(ccc)
+             
+    def latlonfilter(self, llcrnr, urcrnr):
+        """llcrnr tuple (latitude, longitude)
+           urcrnr tuple (latittude, longitude)
+           removes rows with latitude longitude outside of the box
+           described by llcrnr (lower left corner) and urcrnr (upper right corner)
+        """
+        lat1 = llcrnr[0]
+        lat2 = urcrnr[0] 
+        lon1 = llcrnr[1]
+        lon2 = urcrnr[1] 
+        self.df = self.df[self.df['latitude'] < lat2]
+        self.df = self.df[self.df['latitude'] > lat1]
+        self.df = self.df[self.df['longitude'] > lon1]
+        self.df = self.df[self.df['longitude'] < lon2]
+
+    def timefilter(self, daterange):
+        """daterange tuple (datetime, datetime)"""
+        self.df = self.df[self.df['time'] > daterange[0]]
+        self.df = self.df[self.df['time'] < daterange[1]]
+
+
+    def unique(self, cname='Site_Num'): 
+        sites= list(self.df[cname].unique())
+        return sites
+       
+    def plotloc(self, latlon, var='obs'):
+        dftemp = self.df[self.df['latlon'] == latlon]
+        dftemp.set_index('datetime', inplace=True)
+        series = dftemp[var]
+        plt.plot(series, '-b.')
+        plt.title('Site ' + str(latlon[2]) + ' location ' + str(latlon[0]) + ' : ' + str(latlon[1]) )
+        unitlist = dftemp['Units'].unique()
+        #for unit in unitlist:
+        #    unitstr += unit 
+        plt.show()
+    
+       
