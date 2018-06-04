6b62baac40355422816b87618bd4ebd009fcebd1
bbakernoaa <barry.baker@noaa.gov>
2018-06-04 11:11:37 -0400

    Initiallizing observation classes ahead of time

--- monet/obs/__init__.py
+++ monet/obs/__init__.py
@@ -2,6 +2,15 @@ from __future__ import absolute_import, print_function
 
 from . import aeronet, airnow, aqs, crn, epa_util, improve, ish, tolnet
 
-__all__ = ['aeronet', 'airnow', 'aqs', 'crn', 'epa_util', 'improve', 'ish', 'tolnet']
+__all__ = [
+    'aeronet', 'airnow', 'aqs', 'crn', 'epa_util', 'improve', 'ish', 'tolnet'
+]
 
 __name__ = 'obs'
+
+airnow = airnow.AirNow()
+aqs = aqs.AQS()
+aeronet = aeronet.AERONET()
+crn = crn.crn()
+improve = improve.IMPROVE()
+tolnet = tolnet.TOLNet()
