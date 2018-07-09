eec57ea7bd4cbfda56243201bb09c81a261005f2
bbakernoaa <barry.baker@noaa.gov>
2018-07-09 11:38:51 -0400

    init fix

--- monet/models/__init__.py
+++ monet/models/__init__.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import, print_function
 
-from . import basemodel, camx, cmaq, hysplit
+from . import basemodel, camx_mod, cmaq_mod, hysplit_mod
 
-__all__ = ['camx', 'cmaq', 'basemodel', 'hysplit']
+__all__ = ['camx_mod', 'cmaq_mod', 'basemodel', 'hysplit_mod']
 
 __name__ = 'models'
 
-camx = camx.CAMx()
-cmaq = cmaq.CMAQ()
-hysplit = hysplit.HYSPLIT()
+camx = camx_mod.CAMx()
+cmaq = cmaq_mod.CMAQ()
+hysplit = hysplit_mod.HYSPLIT()
