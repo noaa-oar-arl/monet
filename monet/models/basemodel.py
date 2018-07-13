c1f62fd1e16a7c5a443b762b6f96a9dc01135301
bbakernoaa <barry.baker@noaa.gov>
2018-07-13 15:46:53 -0400

    radical update

--- monet/models/basemodel.py
+++ monet/models/basemodel.py
@@ -7,34 +7,145 @@ from gc import collect
 import pandas as pd
 import xarray as xr
 from dask.diagnostics import ProgressBar
-from numpy import array, zeros
-from past.utils import old_div
+try:
+    from osgeo import osr
+    has_gdal = True
+except ImportError:
+    has_gdal = False
 
 ProgressBar().register()
 
 # TO DO - Need to decide what belongs in the BaseModel. This is just a first guess.
 
+# class BaseModel(object):
+#     def __init__(self):
+#         self.dset = None  # CMAQ xarray dataset object
+#         self.dates = None
+#         self.keys = None
+#         self.indexdates = None
+#         self.latitude = None
+#         self.longitude = None
+#         self.map = None
+#
+#     def check_z(self, varname):
+#         if pd.Series(self.dset[varname].dims).isin(['z']).max():
+#             return True
+#         else:
+#             return False
+#
+#     def get_var(self, varname, mapping_dict=None):
+#         if mapping_dict is not None:
+#             return self.dset[varname]
+#         else:
+#             return self.dset[varname]
 
+
+def _ioapi_grid_from_dataset(ds):
+    """Get the IOAPI projection out of the file into proj4."""
+
+    pargs = dict()
+    # Normal WRF file
+    cen_lon = ds.XCENT
+    cen_lat = ds.YCENT
+    dx = ds.XCELL
+    dy = ds.YCELL
+    pargs['lat_1'] = ds.P_ALP
+    pargs['lat_2'] = ds.P_BET
+    pargs['lat_0'] = ds.YCENT
+    pargs['lon_0'] = ds.P_GAM
+    pargs['center_lon'] = ds.XCENT
+    proj_id = ds.GDTYP
+    atol = 1e-4
+    if proj_id == 2:
+        # Lambert
+        p4 = '+proj=lcc +lat_1={lat_1} +lat_2={lat_2} ' \
+             '+lat_0={lat_0} +lon_0={lon_0} ' \
+             '+x_0=0 +y_0=0 +a=6370000 +b=6370000'
+        p4 = p4.format(**pargs)
+    elif proj_id == 4:
+        # Polar stereo
+        p4 = '+proj=stere +lat_ts={lat_1} +lon_0={lon_0} +lat_0=90.0' \
+             '+x_0=0 +y_0=0 +a=6370000 +b=6370000'
+        p4 = p4.format(**pargs)
+        # pyproj and WRF do not agree well close to the pole
+        atol = 5e-3
+    elif proj_id == 3:
+        # Mercator
+        p4 = '+proj=merc +lat_ts={lat_1} ' \
+             '+lon_0={center_lon} ' \
+             '+x_0=0 +y_0=0 +a=6370000 +b=6370000'
+        p4 = p4.format(**pargs)
+    else:
+        raise NotImplementedError('IOAPI proj not implemented yet: '
+                                  '{}'.format(proj_id))
+
+    return p4
+
+
+def grid_from_dataset(ds):
+    """Find out if the dataset contains enough info for Salem to understand.
+
+    ``ds`` can be an xarray dataset
+
+    Returns a :py:string:`proj4_string` if successful, ``None`` otherwise
+    """
+    # maybe its an IOAPI file
+    if hasattr(ds, 'IOAPI_VERSION') or hasattr(ds, 'P_ALP'):
+        # IOAPI_VERSION
+        return _ioapi_grid_from_dataset(ds)
+
+    # Try out platte carree
+    return _lonlat_grid_from_dataset(ds)
+
+
+@xr.register_dataarray_accessor('monet')
 class BaseModel(object):
-    def __init__(self):
-        self.dset = None  # CMAQ xarray dataset object
-        self.dates = None
-        self.keys = None
-        self.indexdates = None
-        self.latitude = None
-        self.longitude = None
-        self.map = None
+    def __init__(self, xray_obj):
+        self.obj = xray_obj
 
-    # def open_files(self, flist=None):
-    #     """Adds information from files to the dset xarray"""
-    #     for fname in flist:
-    #         self.add_files(fname)
+        def interpz(self,
+                    zcoord,
+                    levels,
+                    dim_name='',
+                    fill_value=np.NaN,
+                    use_multiprocessing=True):
+            """Interpolates the array along the vertical dimension
 
-    def check_z(self, varname):
-        if pd.Series(self.dset[varname].dims).isin(['z']).max():
-            return True
-        else:
-            return False
+            Parameters
+            ----------
+            zcoord: DataArray
+              the z coordinates of the variable. Must be of same dimensions
+            levels: 1dArray
+              the levels at which to interpolate
+            dim_name: str
+              the name of the new dimension
+            fill_value : np.NaN or 'extrapolate', optional
+              how to handle levels below the topography. Default is to mark them
+              as invalid, but you might want the have them extrapolated.
+            use_multiprocessing: bool
+              set to false if, for some reason, you don't want to use mp
 
-    def get_var(self, varname):
-        return self.dset[varname]
+            Returns
+            -------
+            a new DataArray with the interpolated data
+            """
+            print('TODO')
+
+    def cartopy(self):
+        """Returns a cartopy.crs.Projection for this dataset."""
+        return proj_to_cartopy(self.obj.proj4_srs)
+
+
+def open_ioapi_dataset(fname):
+    from pandas import Series
+    import ioapitools
+    dset = xr.open_mfdataset(fname)
+    grid = grid_from_dataset(dset)
+    for i in dset.variables:
+        dset[i].assign_attrs({'proj4_srs': grid})
+    lazy_vars = Series(dir(ioapitools)).str.contains('add_lazy')
+    for m in Series(dir(ioapitools)).loc[lazy_vars]:
+        print(m)
+        meth = getattr(ioapitools, m)
+        dset = meth(dset)
+    return dset
---
+++ monet/models/ioapitools.py
@@ -0,0 +1,137 @@
+from numpy import array, concatenate
+from pandas import Series
+
+
+def can_do(index):
+    if index.max():
+        return True
+    else:
+        return False
+
+
+def add_lazy_pm25(d):
+    keys = Series([i for i in d.variables])
+    allvars = Series(concatenate([aitken, accumulation, coarse]))
+    weights = Series([
+        1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
+        1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
+        1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
+        1., 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2
+    ])
+    if 'PM25_TOT' in keys:
+        d['PM25'] = d['PM25_TOT'].chunks()
+    else:
+        index = allvars.isin(keys)
+        newkeys = allvars.loc[index]
+        newweights = weights.loc[index]
+        d['PM25'] = add_multiple_lazy(d, newkeys, weights=newweights)
+        d['PM25'].assign_attrs({'name': 'PM2.5', 'long_name': 'PM2.5'})
+    return d
+
+
+def add_lazy_pm10(d):
+    keys = Series([i for i in d.variables])
+    allvars = Series(concatenate([aitken, accumulation, coarse]))
+    if 'PM_TOT' in keys:
+        d['PM10'] = d['PM_TOT'].chunks()
+    else:
+        index = allvars.isin(keys)
+        if can_do(index):
+            newkeys = allvars.loc[index]
+            d['PM10'] = add_multiple_lazy(d, newkeys)
+            d['PM10'] = add_multiple_lazy(d, newkeys)
+            d['PM10'].assign_attrs({
+                'name':
+                'PM10',
+                'long_name':
+                'Particulate Matter < 10 microns'
+            })
+    return d
+
+
+def add_lazy_pm_course(d):
+    keys = Series([i for i in d.variables])
+    allvars = Series(coarse)
+    index = allvars.isin(keys)
+    if can_do(index):
+        newkeys = allvars.loc[index]
+        d['PM_COURSE'] = add_multiple_lazy(d, newkeys)
+        d['PM_COURSE'].assign_attrs({
+            'name':
+            'PM_COURSE',
+            'long_name':
+            'Course Mode Particulate Matter'
+        })
+    return d
+
+
+def add_lazy_clf(d):
+    keys = Series([i for i in d.variables])
+    allvars = Series(['ACLI', 'ACLJ', 'ACLK'])
+    weights = Series([1, 1, .2])
+    index = allvars.isin(keys)
+    if can_do(index):
+        newkeys = allvars.loc[index]
+        neww = weights.loc[index]
+        d['CLf'] = add_multiple_lazy(d, newkeys, weights=neww)
+        d['CLf'].attrs = d[newkeys[0]].attrs
+    return d
+
+
+def add_lazy_naf(d):
+    keys = Series([i for i in d.variables])
+    allvars = Series(['ANAI', 'ANAJ', 'ASEACAT', 'ASOIL', 'ACORS'])
+    weights = Series(
+        [1, 1, .2 * 837.3 / 1000., .2 * 62.6 / 1000., .2 * 2.3 / 1000.])
+    index = allvars.isin(keys)
+    if can_do(index):
+        newkeys = allvars.loc[index]
+        neww = weights.loc[index]
+        d['NAf'] = add_multiple_lazy(d, newkeys, weights=neww)
+        d['NAf'].assign_attrs({'name': 'NAf', 'long_name': 'NAf'})
+    return d
+
+
+def add_multiple_lazy(dset, variables, weights=None):
+    from numpy import ones
+    if weights is None:
+        weights = ones(len(variables))
+    new = dset[variables[0]].copy() * weights[0]
+    for i, j in zip(variables[1:], weights[1:]):
+        new += dset[i].chunk() * j
+    return new
+
+
+accumulation = array([
+    'AALJ', 'AALK1J', 'AALK2J', 'ABNZ1J', 'ABNZ2J', 'ABNZ3J', 'ACAJ', 'ACLJ',
+    'AECJ', 'AFEJ', 'AISO1J', 'AISO2J', 'AISO3J', 'AKJ', 'AMGJ', 'AMNJ',
+    'ANAJ', 'ANH4J', 'ANO3J', 'AOLGAJ', 'AOLGBJ', 'AORGCJ', 'AOTHRJ', 'APAH1J',
+    'APAH2J', 'APAH3J', 'APNCOMJ', 'APOCJ', 'ASIJ', 'ASO4J', 'ASQTJ', 'ATIJ',
+    'ATOL1J', 'ATOL2J', 'ATOL3J', 'ATRP1J', 'ATRP2J', 'AXYL1J', 'AXYL2J',
+    'AXYL3J', 'AORGAJ', 'AORGPAJ', 'AORGBJ'
+])
+aitken = array([
+    'ACLI', 'AECI', 'ANAI', 'ANH4I', 'ANO3I', 'AOTHRI', 'APNCOMI', 'APOCI',
+    'ASO4I', 'AORGAI', 'AORGPAI', 'AORGBI'
+])
+coarse = array(
+    ['ACLK', 'ACORS', 'ANH4K', 'ANO3K', 'ASEACAT', 'ASO4K', 'ASOIL'])
+noy_gas = array([
+    'NO', 'NO2', 'NO3', 'N2O5', 'HONO', 'HNO3', 'PAN', 'PANX', 'PNA', 'NTR',
+    'CRON', 'CRN2', 'CRNO', 'CRPX', 'OPAN'
+])
+pec = array(['AECI', 'AECJ'])
+pso4 = array(['ASO4I', 'ASO4J'])
+pno3 = array(['ANO3I', 'ANO3J'])
+pnh4 = array(['ANH4I', 'ANH4J'])
+pcl = array(['ACLI', 'ACLJ'])
+poc = array([
+    'AOTHRI', 'APNCOMI', 'APOCI', 'AORGAI', 'AORGPAI', 'AORGBI', 'ATOL1J',
+    'ATOL2J', 'ATOL3J', 'ATRP1J', 'ATRP2J', 'AXYL1J', 'AXYL2J', 'AXYL3J',
+    'AORGAJ', 'AORGPAJ', 'AORGBJ', 'AOLGAJ', 'AOLGBJ', 'AORGCJ', 'AOTHRJ',
+    'APAH1J', 'APAH2J', 'APAH3J', 'APNCOMJ', 'APOCJ', 'ASQTJ', 'AISO1J',
+    'AISO2J', 'AISO3J', 'AALK1J', 'AALK2J', 'ABNZ1J', 'ABNZ2J', 'ABNZ3J',
+    'AORGAI', 'AORGAJ', 'AORGPAI', 'AORGPAJ', 'AORGBI', 'AORGBJ'
+])
+minerals = array(
+    ['AALJ', 'ACAJ', 'AFEJ', 'AKJ', 'AMGJ', 'AMNJ', 'ANAJ', 'ATIJ', 'ASIJ'])
