ce9db7aa4c2da40dbe504a48655a2a47a1d9930c
bbakernoaa <barry.baker@noaa.gov>
2018-08-28 14:38:45 -0400

    adding monet_accessor to __init__

--- monet/__init__.py
+++ monet/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, print_function
 
 from . import models, obs, plots, util, verification
-from .monet import *
+from .monet_accessor import MONETAccessor
 
 # from .monetmodels, obs, plots, util
-__all__ = ['monet', 'models', 'obs', 'plots', 'verification', 'util']
+__all__ = ['models', 'obs', 'plots', 'verification', 'util']
