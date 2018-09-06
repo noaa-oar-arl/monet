7e3551a8bda14225f91e98cd53eb453f3b92f50e
bbakernoaa <barry.baker@noaa.gov>
2018-09-06 12:42:56 -0400

    adding doc blocks

--- monet/models/cmaq.py
+++ monet/models/cmaq.py
@@ -170,6 +170,19 @@ def add_lazy_pm25(d):
 
 
 def add_lazy_pm10(d):
+    """Short summary.
+
+    Parameters
+    ----------
+    d : type
+        Description of parameter `d`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     keys = Series([i for i in d.variables])
     allvars = Series(concatenate([aitken, accumulation, coarse]))
     if 'PM_TOT' in keys:
@@ -191,6 +204,19 @@ def add_lazy_pm10(d):
 
 
 def add_lazy_pm_course(d):
+    """Short summary.
+
+    Parameters
+    ----------
+    d : type
+        Description of parameter `d`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     keys = Series([i for i in d.variables])
     allvars = Series(coarse)
     index = allvars.isin(keys)
@@ -209,6 +235,19 @@ def add_lazy_pm_course(d):
 
 
 def add_lazy_clf(d):
+    """Short summary.
+
+    Parameters
+    ----------
+    d : type
+        Description of parameter `d`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     keys = Series([i for i in d.variables])
     allvars = Series(['ACLI', 'ACLJ', 'ACLK'])
     weights = Series([1, 1, .2])
@@ -229,6 +268,19 @@ def add_lazy_clf(d):
 
 
 def add_lazy_caf(d):
+    """Short summary.
+
+    Parameters
+    ----------
+    d : type
+        Description of parameter `d`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     keys = Series([i for i in d.variables])
     allvars = Series(['ACAI', 'ACAJ', 'ASEACAT', 'ASOIL', 'ACORS'])
     weights = Series(
@@ -250,6 +302,19 @@ def add_lazy_caf(d):
 
 
 def add_lazy_naf(d):
+    """Short summary.
+
+    Parameters
+    ----------
+    d : type
+        Description of parameter `d`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     keys = Series([i for i in d.variables])
     allvars = Series(['ANAI', 'ANAJ', 'ASEACAT', 'ASOIL', 'ACORS'])
     weights = Series(
@@ -268,6 +333,19 @@ def add_lazy_naf(d):
 
 
 def add_lazy_so4f(d):
+    """Short summary.
+
+    Parameters
+    ----------
+    d : type
+        Description of parameter `d`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     keys = Series([i for i in d.variables])
     allvars = Series(['ASO4I', 'ASO4J', 'ASO4K'])
     weights = Series([1., 1., .2])
@@ -285,6 +363,19 @@ def add_lazy_so4f(d):
 
 
 def add_lazy_nh4f(d):
+    """Short summary.
+
+    Parameters
+    ----------
+    d : type
+        Description of parameter `d`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     keys = Series([i for i in d.variables])
     allvars = Series(['ANH4I', 'ANH4J', 'ANH4K'])
     weights = Series([1., 1., .2])
@@ -302,6 +393,19 @@ def add_lazy_nh4f(d):
 
 
 def add_lazy_no3f(d):
+    """Short summary.
+
+    Parameters
+    ----------
+    d : type
+        Description of parameter `d`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     keys = Series([i for i in d.variables])
     allvars = Series(['ANO3I', 'ANO3J', 'ANO3K'])
     weights = Series([1., 1., .2])
@@ -319,6 +423,19 @@ def add_lazy_no3f(d):
 
 
 def add_lazy_noy(d):
+    """Short summary.
+
+    Parameters
+    ----------
+    d : type
+        Description of parameter `d`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     keys = Series([i for i in d.variables])
     allvars = Series(noy_gas)
     index = allvars.isin(keys)
@@ -330,6 +447,19 @@ def add_lazy_noy(d):
 
 
 def add_lazy_rh(d):
+    """Short summary.
+
+    Parameters
+    ----------
+    d : type
+        Description of parameter `d`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     # keys = Series([i for i in d.variables])
     # allvars = Series(['TEMP', 'Q', 'PRES'])
     # index = allvars.isin(keys)
@@ -346,6 +476,19 @@ def add_lazy_rh(d):
 
 
 def add_lazy_nox(d):
+    """Short summary.
+
+    Parameters
+    ----------
+    d : type
+        Description of parameter `d`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     keys = Series([i for i in d.variables])
     allvars = Series(['NO', 'NOX'])
     index = allvars.isin(keys)
@@ -357,6 +500,23 @@ def add_lazy_nox(d):
 
 
 def add_multiple_lazy(dset, variables, weights=None):
+    """Short summary.
+
+    Parameters
+    ----------
+    dset : type
+        Description of parameter `dset`.
+    variables : type
+        Description of parameter `variables`.
+    weights : type
+        Description of parameter `weights`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     from numpy import ones
     if weights is None:
         weights = ones(len(variables))
@@ -385,7 +545,6 @@ def _predefined_mapping_tables(dset):
         'NOY': ['NOy'],
         'NOX': ['NOx'],
         'SO2': ['SO2'],
-        'NOX': ['NOx'],
         'NO': ['NO'],
         'NO2': ['NO2'],
         'SO4f': ['SO4f'],
@@ -419,7 +578,6 @@ def _predefined_mapping_tables(dset):
         'NOY': ['NOy'],
         'NOX': ['NOx'],
         'SO2': ['SO2'],
-        'NOX': ['NOx'],
         'NO': ['NO'],
         'NO2': ['NO2'],
         'SO4f': ['SO4f'],
