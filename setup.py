b61cd4fcb9521a6514d7aba70119e465835eff67
Barry Baker <bbakernoaa@users.noreply.github.com>
2018-11-06 15:47:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     ],
     description='The Model and Observation Evaluation Toolkit (MONET)',
     install_requires=[
-        'numpy', 'pandas', 'pyresample', 'netcdf4', 'xarray', 'dask',
+        'numpy>=1.6', 'pandas', 'pyresample', 'netcdf4', 'xarray', 'dask',
         'matplotlib', 'seaborn', 'pseudonetcdf', 'cython', 'cartopy', 'future', 'sphinx',
         'pandoc'],
     dependency_links=[
