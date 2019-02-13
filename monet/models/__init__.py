c3103c804eeca884765f7397bffb9c3bd4aa304b
bbakernoaa <barry@Barrys-MacBook-Pro.local>
2019-02-13 10:27:56 -0500

    removing combinetools from monet.models

--- monet/models/__init__.py
+++ monet/models/__init__.py
@@ -1,5 +1,5 @@
-from . import cmaq, hysplit, camx, combinetool, fv3chem
+from . import cmaq, hysplit, camx, fv3chem
 
-__all__ = ['cmaq', 'hysplit', 'camx', 'combinetool', 'fv3cehm']
+__all__ = ['cmaq', 'hysplit', 'camx', 'fv3chem']
 
 __name__ = 'models'
