58630047713b590f52a2235f7cf0233ba3b7ac2a
bbakernoaa <barry.baker@noaa.gov>
2019-03-31 17:27:59 -0400

    fix #

--- docs/conf.py
+++ docs/conf.py
@@ -127,14 +127,14 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+# html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+# html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+# html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
