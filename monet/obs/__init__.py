5cd1f2f4aa5ef7bea1cac494274a7f0b14db5749
Barry Baker <bbakernoaa@users.noreply.github.com>
2019-05-05 04:26:40 -0400

    Update __init__.py

--- monet/obs/__init__.py
+++ monet/obs/__init__.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import, print_function
 # from . import aeronet, airnow_mod, aqs_mod, crn_mod, epa_util, improve_mod
 # from . import ish_mod, tolnet_mod, cems_mod, nadp_mod, modis_swath, icartt_mod
-from . import aeronet, airnow, aqs_mod, crn_mod, epa_util, improve_mod
+from . import aeronet, airnow, aqs, crn_mod, epa_util, improve_mod
 from . import ish, cems_mod, nadp_mod, icartt_mod
 
 __all__ = [
-    'aeronet', 'airnow', 'aqs_mod', 'crn_mod', 'epa_util',
+    'aeronet', 'airnow', 'aqs', 'crn_mod', 'epa_util',
     'improve_mod', 'ish', 'cems_mod', 'nadp_mod',
     'icartt_mod'
 ]
@@ -14,7 +14,7 @@ __name__ = 'obs'
 
 # ish = ish_mod.ISH()
 # airnow = airnow_mod.AirNow()
-aqs = aqs_mod.AQS()
+# aqs = aqs_mod.AQS()
 # aeronet = aeronet_mod.AERONET()
 crn = crn_mod.crn()
 improve = improve_mod.IMPROVE()
