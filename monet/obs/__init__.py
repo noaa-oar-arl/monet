24e32c709a2e400cf510cae7c0978dc1a1e12826
bbakernoaa <barry.baker@noaa.gov>
2019-05-05 11:04:22 -0400

    adding xarray outside icartt initialization

--- monet/obs/__init__.py
+++ monet/obs/__init__.py
@@ -1,13 +1,13 @@
 from __future__ import absolute_import, print_function
+
 # from . import aeronet, airnow_mod, aqs_mod, crn_mod, epa_util, improve_mod
 # from . import ish_mod, tolnet_mod, cems_mod, nadp_mod, modis_swath, icartt_mod
-from . import aeronet, airnow, aqs, crn, epa_util, improve_mod
-from . import ish, cems_mod, nadp_mod, icartt_mod
+from . import (aeronet, airnow, aqs, cems_mod, crn, epa_util, icartt,
+               improve_mod, ish, nadp_mod)
 
 __all__ = [
-    'aeronet', 'airnow', 'aqs', 'crn', 'epa_util',
-    'improve_mod', 'ish', 'cems_mod', 'nadp_mod',
-    'icartt_mod'
+    'aeronet', 'airnow', 'aqs', 'crn', 'epa_util', 'improve_mod', 'ish',
+    'cems_mod', 'nadp_mod', 'icartt'
 ]
 
 __name__ = 'obs'
@@ -21,4 +21,4 @@ improve = improve_mod.IMPROVE()
 # tolnet = tolnet_mod.TOLNet()
 cems = cems_mod.CEMS()
 nadp = nadp_mod.NADP()
-icartt = icartt_mod.icartt()
+#icartt = icartt_mod.icartt()
