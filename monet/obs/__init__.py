999ac10aeed5c571703ec535730cd25723ade908
Barry Baker <bbakernoaa@users.noreply.github.com>
2019-05-05 04:10:23 -0400

    Update __init__.py

--- monet/obs/__init__.py
+++ monet/obs/__init__.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import, print_function
-# from . import aeronet_mod, airnow_mod, aqs_mod, crn_mod, epa_util, improve_mod
+# from . import aeronet, airnow_mod, aqs_mod, crn_mod, epa_util, improve_mod
 # from . import ish_mod, tolnet_mod, cems_mod, nadp_mod, modis_swath, icartt_mod
-from . import aeronet_mod, airnow, aqs_mod, crn_mod, epa_util, improve_mod
+from . import aeronet, airnow, aqs_mod, crn_mod, epa_util, improve_mod
 from . import ish_mod, cems_mod, nadp_mod, icartt_mod
 
 __all__ = [
-    'aeronet_mod', 'airnow', 'aqs_mod', 'crn_mod', 'epa_util',
+    'aeronet', 'airnow', 'aqs_mod', 'crn_mod', 'epa_util',
     # 'improve_mod', 'ish_mod', 'tolnet_mod', 'cems_mod', 'nadp_mod',
     'improve_mod', 'ish_mod', 'cems_mod', 'nadp_mod',
     # 'modis_swath', 'icartt_mod'
@@ -17,7 +17,7 @@ __name__ = 'obs'
 ish = ish_mod.ISH()
 # airnow = airnow_mod.AirNow()
 aqs = aqs_mod.AQS()
-aeronet = aeronet_mod.AERONET()
+# aeronet = aeronet_mod.AERONET()
 crn = crn_mod.crn()
 improve = improve_mod.IMPROVE()
 # tolnet = tolnet_mod.TOLNet()
