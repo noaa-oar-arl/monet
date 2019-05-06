1b730fb1d301280feb9bc6524d5b648eec78ad35
bbakernoaa <barry.baker@noaa.gov>
2019-05-06 09:19:31 -0400

    fixing

--- monet/obs/__init__.py
+++ monet/obs/__init__.py
@@ -1,27 +1,24 @@
 from __future__ import absolute_import, print_function
 
-# from . import aeronet_mod, airnow_mod, aqs_mod, crn_mod, epa_util, improve_mod
+# from . import aeronet, airnow_mod, aqs_mod, crn_mod, epa_util, improve_mod
 # from . import ish_mod, tolnet_mod, cems_mod, nadp_mod, modis_swath, icartt_mod
-from . import (aeronet_mod, airnow, aqs_mod, cems_mod, crn_mod, epa_util,
-               icartt_mod, improve_mod, ish_mod, nadp_mod)
+from . import (aeronet, airnow, aqs, cems_mod, crn, epa_util, icartt,
+               improve_mod, ish, nadp_mod)
 
 __all__ = [
-    'aeronet_mod', 'airnow', 'aqs_mod', 'crn_mod', 'epa_util',
-    # 'improve_mod', 'ish_mod', 'tolnet_mod', 'cems_mod', 'nadp_mod',
-    'improve_mod', 'ish_mod', 'cems_mod', 'nadp_mod',
-    # 'modis_swath', 'icartt_mod'
-    'icartt_mod'
+    'aeronet', 'airnow', 'aqs', 'crn', 'epa_util', 'improve_mod', 'ish',
+    'cems_mod', 'nadp_mod', 'icartt'
 ]
 
 __name__ = 'obs'
 
-ish = ish_mod.ISH()
+# ish = ish_mod.ISH()
 # airnow = airnow_mod.AirNow()
-aqs = aqs_mod.AQS()
-aeronet = aeronet_mod.AERONET()
-crn = crn_mod.crn()
+# aqs = aqs_mod.AQS()
+# aeronet = aeronet_mod.AERONET()
+# crn = crn_mod.crn()
 improve = improve_mod.IMPROVE()
 # tolnet = tolnet_mod.TOLNet()
 cems = cems_mod.CEMS()
-nadp = nadp_mod.NADP()
-icartt = icartt_mod.icartt()
+# nadp = nadp_mod.NADP()
+# icartt = icartt_mod.icartt()
