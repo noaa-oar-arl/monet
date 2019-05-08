3693183339983ee4e850cb2e6ddb6f2eaf9c3ead
amcz <alicec@umd.edu>
2019-05-08 16:14:20 -0400

    aqs.py merging now done on siteid, latitutde, and longitude

--- monet/obs/aqs.py
+++ monet/obs/aqs.py
@@ -390,7 +390,10 @@ class AQS(object):
                 [network])].drop_duplicates(subset=['siteid'])
         else:
             monitors = self.monitor_df.drop_duplicates(subset=['siteid'])
-        self.df = pd.merge(self.df, monitors, on=['siteid'], how='left')
+        #AMC - merging only on siteid was causing latitude_x latitude_y to be
+        #created.
+        mlist=['siteid','latitude','longitude']
+        self.df = pd.merge(self.df, monitors, on=mlist, how='left')
         if daily:
             self.df['time'] = self.df.time_local - pd.to_timedelta(
                 self.df.gmt_offset, unit='H')
