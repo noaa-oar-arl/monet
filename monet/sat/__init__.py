2a5d887caace9076f1c11e6598f013922230312e
bbakernoaa <barry.baker@noaa.gov>
2019-05-13 14:14:37 -0400

    updates

--- monet/sat/__init__.py
+++ monet/sat/__init__.py
@@ -1,5 +1,9 @@
-from . import modis_swath, nesdis_edr_viirs, nesdis_eps_viirs, nesdis_frp
+from . import (modis_ornl, modis_swath, nesdis_edr_viirs, nesdis_eps_viirs,
+               nesdis_frp)
 
-__all__ = ['modis_swath', 'nesdis_edr_viirs', 'nesdis_eps_viirs', 'nesdis_frp']
+__all__ = [
+    'modis_swath', 'nesdis_edr_viirs', 'nesdis_eps_viirs', 'nesdis_frp',
+    'modis_ornl'
+]
 
 __name__ = 'sat'
