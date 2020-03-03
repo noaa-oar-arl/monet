183b0a14130b513ff007eca5faaff4c827fa2846
amcz <alicec@umd.edu>
2020-03-03 13:36:16 -0500

    updated hcontrol.py. added message_parse.py, metfiles.py, metexamples.py vmixing.py

---
+++ monet/utilhysplit/metexamples.py
@@ -0,0 +1,22 @@
+import monet.utilhysplit.metfiles as mf
+import datetime
+
+d1 = datetime.datetime(2017,12,5,2)
+runtime = 50
+mdir = '/pub/archives/'
+m2dir = mdir + 'wrf27km/%Y/wrfout_d01_%Y%m%d.ARL'
+files = mf.getmetfiles(m2dir, d1, runtime)
+for fl in files:
+    print(fl)
+
+mdir = '/pub/archives/'
+m2dir = mdir + 'gdas0p5/%Y%m%d_gdas0p5'
+files = mf.getmetfiles(m2dir, d1, runtime)
+for fl in files:
+    print(fl)
+
+mdir = '/pub/archives/'
+m2dir = mdir + 'gdas1/gdas1.%b%d.week'
+files = mf.getmetfiles(m2dir, d1, runtime)
+for fl in files:
+    print(fl)
