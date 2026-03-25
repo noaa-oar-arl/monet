a2eabe64785520cd1cb3837bb88351dc2b6c8688
zmoon <zmoon92@gmail.com>
2026-03-25 14:17:53 -0500

    Use ds `.sizes` in test
    FutureWarning: The return type of `Dataset.dims` will be changed to return a set of dimension names in future, in order to be more consistent with `DataArray.dims`. To access a mapping from dimension names to lengths, please use `Dataset.sizes`.

--- tests/test_remap.py
+++ tests/test_remap.py
@@ -100,7 +100,7 @@ def test_combine_da_da():
     new = combine_da_to_da(model, obs, merge=False, interp_time=False)
 
     # Check
-    assert new.dims == {"z": 5, "y": n, "x": n}
+    assert new.sizes == {"z": 5, "y": n, "x": n}
     assert float(new.longitude.min()) == pytest.approx(0.1)
     assert float(new.longitude.max()) == pytest.approx(0.9)
     assert float(new.latitude.min()) == pytest.approx(0.1)
@@ -112,6 +112,6 @@ def test_combine_da_da():
 
     # Use orthogonal selection to get track
     a = new.data.values[:, new.y, new.x]
-    assert a.shape == (model.dims["z"], n), "model levels but obs grid points"
+    assert a.shape == (model.sizes["z"], n), "model levels but obs grid points"
     assert (np.diff(a.mean(axis=0)) >= 0).all(), "obs profile goes S"
     assert np.isclose(np.diff(a.mean(axis=1)), 1, atol=1e-15, rtol=0).all(), "obs profile goes U"
